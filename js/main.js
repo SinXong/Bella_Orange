c36ec749b6d7f7c68bca1fa49c8ba60549f2a344
[O]ska[R] <vova.kuzneczov.99@gmail.com>
2021-09-14 19:36:08 +0300

    add new screen resolution

--- js/main.js
+++ js/main.js
@@ -7,5 +7,15 @@ $(function() {
                 
         })
         $('.header__menu').slideToggle(0500)
-    })
+    });
+    
+    if($(document).width() > 1440){
+        var $x = $('.foodApp__img').height();
+        var $y = $('.download__img').height();
+        $('.foodApp__container').css("height",$x);
+        $('.download__container').css("height",$y);
+        $('.download').css("height",$y);
+        $('.foodApp__telephone').css("margin-top",($x - 150));
+    }
+
 });
